0bf34e7772b2102fb4db01909935a05310d8f16c
Tingzi123 <1044373556@qq.com>
2020-11-15 23:36:44 +0800

    feat all test

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,14 +1,23 @@
+import axios from "axios";
 import { register } from "../user";
+import { verifyUsername } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
 
 describe("register", () => {
-  test("should post user when validated", () => {
+  test("should post user when validated", async () => {
     // TODO 19: add test here
+    axios.post.mockResolvedValue({data:{username:"username",password:"password"}});
+    await expect(register("username", "password")).resolves.toEqual({
+      username: "username",
+      password: "password",
+    });
   });
 
-  test("should reject with Error when username is invalid", () => {
+  test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
+    verifyUsername.mockReturnValue(false);
+    await expect(register("username", "password")).rejects.toThrow("wrong username or password");
   });
 });
